df41bb0de92697e67a54a42e636f870b78610d78
nurkhaliqfm <nurkhaliqfuthra@gmail.com>
2024-05-10 19:52:08 +0800

    commit

--- models/ref_kode_option.js
+++ models/ref_kode_option.js
@@ -2,6 +2,10 @@ module.exports = (sequelize, DataTypes) => {
   const RefKodeOption = sequelize.define(
     "ref_kode_option",
     {
+      id_kode_soal: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
       name: {
         type: DataTypes.TEXT,
         allowNull: false,
@@ -11,6 +15,15 @@ module.exports = (sequelize, DataTypes) => {
       freezeTableName: true,
     }
   );
+
+  RefKodeOption.associate = (models) => {
+    RefKodeOption.belongsTo(models.ref_kode_soal, {
+      foreignKey: {
+        name: "id_kode_soal",
+      },
+      as: "ref_kode_soal",
+    });
+  };
 
   return RefKodeOption;
 };
