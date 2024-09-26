01fb87201dcf1bd1642c679c043c81111237d2df
nurkhaliqfm <nurkhaliqfuthra@gmail.com>
2024-09-26 21:01:02 +0800

    ref: authentication

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:3002",
-  '"http://localhost:3001"',
+	"http://localhost:3000",
+	"http://localhost:3002",
+	"http://localhost:3001",
+	"http://localhost:5173",
 ];
 
 module.exports = allowedOrigins;
--- controllers/oauthController.js
+++ controllers/oauthController.js
@@ -1,9 +1,9 @@
 const {
-  users,
-  biodata_users,
-  oauth_access_tokens,
-  oauth_refresh_tokens,
-  oauth_authorization_codes,
+	users,
+	biodata_users,
+	oauth_access_tokens,
+	oauth_refresh_tokens,
+	oauth_authorization_codes,
 } = require("../models");
 const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcrypt");
@@ -14,138 +14,131 @@ require("dotenv").config();
 var privateKey = fs.readFileSync("./key/private.key");
 
 const userLogin = async (req, res) => {
-  let { username, password, client_id, client_secret, grant_type } = req.body;
-  if (req.headers.authorization) {
-    let decodedCredentials = req.headers.authorization.split(" ");
-    if (decodedCredentials[0] === "Basic") {
-      decodedCredentials = Buffer.from(
-        decodedCredentials[1],
-        "base64"
-      ).toString("utf-8");
+	let { email, password, grant_type } = req.body;
+	const token = req.headers.authorization.split(" ")[1];
+	const [id_client, client_secret] = Buffer.from(token, "base64")
+		.toString("utf-8")
+		.split(":");
 
-      decodedCredentials = decodedCredentials.split(":");
-      client_id = decodedCredentials[0];
-      client_secret = decodedCredentials[1];
-    }
-  }
+	if (!email || !password)
+		return res
+			.status(401)
+			.json({ message: "These credentials do not match our records." });
 
-  if (!username || !password)
-    return res
-      .status(401)
-      .json({ message: "These credentials do not match our records." });
+	const userData = await users.findOne({
+		where: { email: email },
+		include: [
+			{
+				model: biodata_users,
+				as: "biodata_users",
+				attributes: ["id", "full_name"],
+			},
+		],
+	});
 
-  const userData = await users.findOne({
-    where: { email: username },
-    include: [
-      {
-        model: biodata_users,
-        as: "biodata_users",
-        attributes: ["id", "full_name"],
-      },
-    ],
-  });
+	if (!userData)
+		return res
+			.status(401)
+			.json({ message: "These credentials do not match our records." });
 
-  if (!userData)
-    return res
-      .status(401)
-      .json({ message: "These credentials do not match our records." });
+	bcrypt.compare(password, userData.password).then(async (match) => {
+		if (!match)
+			return res
+				.status(401)
+				.json({ message: "These credentials do not match our records." });
 
-  bcrypt.compare(password, userData.password).then(async (match) => {
-    if (!match)
-      return res
-        .status(401)
-        .json({ message: "These credentials do not match our records." });
+		if (!grant_type && grant_type !== process.env.GRANT_TYPE)
+			return res.status(400).json({
+				error: "invalid_request",
+				message:
+					"The authorization grant type is not supported by the authorization server.",
+			});
 
-    if (!grant_type && grant_type !== process.env.GRANT_TYPE)
-      return res.status(400).json({
-        error: "invalid_request",
-        message:
-          "The authorization grant type is not supported by the authorization server.",
-      });
+		if (!id_client && id_client !== process.env.CLIENT_ID)
+			return res.status(400).json({
+				error: "invalid_request",
+				message:
+					"The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed.",
+			});
 
-    if (!client_id && client_id !== process.env.CLIENT_ID)
-      return res.status(400).json({
-        error: "invalid_request",
-        message:
-          "The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed.",
-      });
+		if (!client_secret && client_secret !== process.env.CLIENT_SECRET)
+			return res.status(400).json({
+				error: "invalid_request",
+				message:
+					"The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed.",
+			});
 
-    if (!client_secret && client_secret !== process.env.CLIENT_SECRET)
-      return res.status(400).json({
-        error: "invalid_request",
-        message:
-          "The request is missing a required parameter, includes an invalid parameter value, includes a parameter more than once, or is otherwise malformed.",
-      });
+		console.log("im here");
 
-    const authorization_codes = uuidv4();
-    const access_token_expires_at = Date.now() + 1800 * 1000;
-    const refresh_token_expires_at = Date.now() + 24 * 60 * 60 * 30 * 1000;
+		const authorization_codes = uuidv4();
+		const access_token_expires_at = Date.now() + 1800 * 1000;
+		const refresh_token_expires_at = Date.now() + 24 * 60 * 60 * 30 * 1000;
 
-    try {
-      const createAuthCodes = await oauth_authorization_codes.create({
-        authorization_codes: authorization_codes,
-        id_user: userData.id,
-      });
+		try {
+			const createAuthCodes = await oauth_authorization_codes.create({
+				authorization_codes: authorization_codes,
+				id_user: userData.id,
+			});
 
-      const accessToken = jwt.sign(
-        {
-          UserInfo: {
-            id: userData.id,
-            clientId: client_id,
-            clientSecret: client_secret,
-            id_auth: createAuthCodes.id,
-            grant_type: password,
-          },
-        },
-        privateKey,
-        { expiresIn: "30m", algorithm: "RS256" }
-      );
+			const accessToken = jwt.sign(
+				{
+					UserInfo: {
+						id: userData.id,
+						clientId: id_client,
+						clientSecret: client_secret,
+						id_auth: createAuthCodes.id,
+						grant_type: password,
+					},
+				},
+				privateKey,
+				{ expiresIn: "30m", algorithm: "RS256" }
+			);
 
-      const refreshToken = jwt.sign(
-        {
-          id: userData.id,
-          clientSecret: client_secret,
-          id_auth: createAuthCodes.id,
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: "30d" }
-      );
+			const refreshToken = jwt.sign(
+				{
+					id: userData.id,
+					clientSecret: client_secret,
+					id_auth: createAuthCodes.id,
+				},
+				process.env.REFRESH_TOKEN_SECRET,
+				{ expiresIn: "30d" }
+			);
 
-      await oauth_access_tokens.create({
-        access_token: accessToken,
-        access_token_expires_at: access_token_expires_at,
-        id_auth: createAuthCodes.id,
-      });
+			await oauth_access_tokens.create({
+				access_token: accessToken,
+				access_token_expires_at: access_token_expires_at,
+				id_auth: createAuthCodes.id,
+			});
 
-      await oauth_refresh_tokens.create({
-        refresh_token: refreshToken,
-        refresh_token_expires_at: refresh_token_expires_at,
-        id_auth: createAuthCodes.id,
-      });
+			await oauth_refresh_tokens.create({
+				refresh_token: refreshToken,
+				refresh_token_expires_at: refresh_token_expires_at,
+				id_auth: createAuthCodes.id,
+			});
 
-      res.status(200).json({
-        name: userData.biodata_users.full_name,
-        role: roleConverter(userData.role_id),
-        expires_in: access_token_expires_at,
-        access_token: accessToken,
-      });
-    } catch (error) {
-      res.status(401).json({
-        message: "Authentication failed with this cradentials",
-      });
-    }
-  });
+			res.status(200).json({
+				name: userData.biodata_users.full_name,
+				role: roleConverter(userData.role_id),
+				expires_in: access_token_expires_at,
+				access_token: accessToken,
+			});
+		} catch (error) {
+			res.status(401).json({
+				message: "Authentication failed with this cradentials",
+			});
+		}
+	});
 };
 
 module.exports = { userLogin };
 
 function roleConverter(roleId) {
-  switch (roleId) {
-    case 0:
-      return "Super";
-    case 1:
-      return "Admin";
-    case 2:
-      return "User";
-  }
+	switch (roleId) {
+		case 0:
+			return "Super";
+		case 1:
+			return "Admin";
+		case 2:
+			return "User";
+	}
 }
